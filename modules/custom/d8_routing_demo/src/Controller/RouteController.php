87bf81585792ffdc83e6e673debd8a6667e95be8
senthil <senthil.kv@capgemini.com>
2018-09-26 12:57:55 +0530

    Activity 13-15 Permissions Extended: Creating custom access check

--- modules/custom/d8_routing_demo/src/Controller/RouteController.php
+++ modules/custom/d8_routing_demo/src/Controller/RouteController.php
@@ -3,6 +3,9 @@
 namespace Drupal\d8_routing_demo\Controller;
 
 use Drupal\user\UserInterface;
+use Drupal\node\NodeInterface;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Session\AccountInterface;
 
 class RouteController {
 	public function hello_world() {
@@ -32,4 +35,17 @@ class RouteController {
 		return 'Hello '.$user->getUsername();
 	}
 
-}
+	public function listNode(NodeInterface $node) {
+	    $owner = $node->getOwner()->getAccountName();
+	    return [
+	      '#type' => '#markup',
+	      '#markup' => $node->getTitle() . '|' . $owner,
+	    ];
+  }
+   public function listNodeAccess(NodeInterface $node, AccountInterface $account) {
+    return AccessResult::allowedIf(
+      $node->getOwnerId() === $account->id()
+    );
+  }
+
+ }
